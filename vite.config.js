c7d69194d2c6853f822685a24979a821b1f7b611
JKs162426 <jesus20jk@gmail.com>
2026-02-14 14:11:08 -0700

    vite.config.js multi-page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/glossia/",
+  base: process.env.NODE_ENV === "production" ? "/glossia/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        translator: resolve(__dirname, "translator/index.html"),
+        categories: resolve(__dirname, "categories/index.html"),
+        flashcards: resolve(__dirname, "flashcards/index.html"),
+        favorite_words: resolve(__dirname, "favorite_words/index.html"),
+      },
+    },
+  },
 });
